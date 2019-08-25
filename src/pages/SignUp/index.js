57617406ad42addefbc2093d972955fffd56f770
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-25 16:52:55 -0300

    Pagina SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({navigation}) {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCaptalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
