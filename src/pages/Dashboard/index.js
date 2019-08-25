9d25d09c4c93c29fe94f525f0acefac7ee0a2bce
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-25 19:20:32 -0300

    Estilizaçao das rotas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  return <Background />;
 }
+
+Dashboard.navigationOptions = {
+  // renomeia a aba do Dashboard (label)
+  tabBarLabel: 'Agendamentos',
+  // foi feito em forma de função o icone
+  // pq a cor vem como parametro (tintColor) de dentro do react navigation
+  tabBarIcon: ({tintColor}) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
