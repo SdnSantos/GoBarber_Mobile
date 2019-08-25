ceba5d9a5e4b8509b04e4c4be7d1716fbf691242
Sidney Santos <sidney_ssantos@hotmail.com>
2019-08-25 19:55:39 -0300

    Lista de agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,11 +3,25 @@ import {View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '~/components/Background';
+import Appointment from '~/components/Appointment';
 
-// import { Container } from './styles';
+import {Container, Title, List} from './styles';
 
 export default function Dashboard() {
-  return <Background />;
+  const data = [1, 2, 3, 4, 5];
+
+  return (
+    <Background>
+      <Container>
+        <Title>Agendamentos</Title>
+        <List
+          data={data}
+          keyExtractor={item => String(item)}
+          renderItem={({item}) => <Appointment data={item} />}
+        />
+      </Container>
+    </Background>
+  );
 }
 
 Dashboard.navigationOptions = {
